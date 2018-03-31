65db409d119073d2f8e08006f9e07326d52653b7
lukovnikov <lukovnikov@outlook.com>
2018-03-31 18:52:04 +0200

    lcquad corerank - flat preprocessing

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name="qelos_core",
-      description="qelos-core",
-      author="Sum-Ting Wong",
-      author_email="sumting@wo.ng",
-      install_requires=[],
-      packages=["qelos_core"],
+setup(name="Qelos Core",
+      description="Pytorch stuff",
+      packages=['qelos_core'],
       )
