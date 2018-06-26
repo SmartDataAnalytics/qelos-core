f81b12e20bc46d0f3982488e736f76b5cd83e4a8
lukovnikov <lukovnikov@outlook.com>
2018-06-26 21:20:09 +0200

    fid and inception

--- test/test_gan.py
+++ test/test_gan.py
@@ -11,33 +11,34 @@ class MyTestCase(TestCase):
         self.assertEqual(True, True)
 
 
-class TestInceptionScore(TestCase):
-    def tst_inception_cifar10(self):
-        class IgnoreLabelDataset(torch.utils.data.Dataset):
-            def __init__(self, orig):
-                self.orig = orig
+def tst_inception_cifar10(self):
+    class IgnoreLabelDataset(torch.utils.data.Dataset):
+        def __init__(self, orig):
+            self.orig = orig
 
-            def __getitem__(self, index):
-                return self.orig[index][0]
+        def __getitem__(self, index):
+            return self.orig[index][0]
 
-            def __len__(self):
-                return len(self.orig)
-        cifar = dset.CIFAR10(root='data/', download=True,
-                             transform=transforms.Compose([
-                                 transforms.Scale(32),
-                                 transforms.ToTensor(),
-                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                             ])
-                             )
+        def __len__(self):
+            return len(self.orig)
+    cifar = dset.CIFAR10(root='data/', download=True,
+                         transform=transforms.Compose([
+                             transforms.Scale(32),
+                             transforms.ToTensor(),
+                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                         ])
+                         )
 
-        cifar = IgnoreLabelDataset(cifar)
-        cifar_loader = q.dataload(cifar, batch_size=32)
-        inception_scorer = q.gan.IS()
+    cifar = IgnoreLabelDataset(cifar)
+    cifar_loader = q.dataload(cifar, batch_size=32)
+    inception_scorer = q.gan.IS()
 
-        print ("Calculating Inception Score...")
+    print ("Calculating Inception Score...")
 
-        scores = inception_scorer.get_scores(cifar_loader)
+    scores = inception_scorer.get_scores(cifar_loader)
+    print(scores)
 
 
 if __name__ == '__main__':
     unittest.main()
+    tst_inception_cifar10()
