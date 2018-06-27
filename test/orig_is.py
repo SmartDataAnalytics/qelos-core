0d323023f99fdca7828f4b46db74896fedd8521c
lukovnikov <lukovnikov@outlook.com>
2018-06-27 18:46:04 +0200

    merged fid and is too

--- test/orig_is.py
+++ test/orig_is.py
@@ -47,7 +47,7 @@ def get_inception_score(images, splits=10):
         inp = inps[(i * bs):min((i + 1) * bs, len(inps))]
         inp = np.concatenate(inp, 0)
         inp = torch.nn.functional.upsample(
-                torch.tensor(inp).unsqueeze(0), size=(299, 299), mode="bilinear").numpy()
+                torch.tensor(inp), size=(299, 299), mode="bilinear").numpy()
         pred = sess.run(softmax, {'ExpandDims:0': inp})
         preds.append(pred)
     preds = np.concatenate(preds, 0)
