4ee532959cb9d47a1d709cc52689de9be9d54c18
lukovnikov <lukovnikov@outlook.com>
2018-07-09 18:40:25 +0200

    added decays in gan trainer, changed betas

--- test/test_gan.py
+++ test/test_gan.py
@@ -25,8 +25,8 @@ def tst_inception_cifar10(cuda=False, gpu=1, batsize=32):
                          transform=transforms.Compose([
                              transforms.Scale(32),
                              transforms.ToTensor(),
-                             # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             #                      std=[0.229, 0.224, 0.225])
+                             transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                                  std=[0.5, 0.5, 0.5])
                          ])
                          )
     device = torch.device("cpu") if not cuda else torch.device("cuda", gpu)
