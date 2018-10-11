2eff5d62eec874b21aebb3383410477f7a1b1135
lukovnikov <lukovnikov@outlook.com>
2018-10-11 18:24:16 +0200

    refactored trainer and tester

--- qelos_core/train.py
+++ qelos_core/train.py
@@ -24,89 +24,6 @@ class TensorDataset(PytorchTensorDataset):
         super(TensorDataset, self).__init__(*tensors)
 
 
-class Aggregator(object):
-    """ Normalizes current running numbers """
-    def __init__(self, name=None, mode="mean"):     # TODO: support aggmode "sum"???
-        super(Aggregator, self).__init__()
-        self.aggmode = mode
-        self.current_agg_error = 0.
-        self.current_agg_norma = 0.
-        self.agg_history = []
-        self.agg_epochs = []
-        self.name = name
-
-    def get_agg_error(self):
-        if self.aggmode == "mean":
-            if self.current_agg_norma == 0.:
-                ret = 0.
-            else:
-                ret = self.current_agg_error / max(self.current_agg_norma, 1e-6)
-        else:
-            ret = self.current_agg_error
-        ret = self.post_agg_epoch(ret)
-        return ret
-
-    def post_agg_epoch(self, x):
-        return x
-
-    def push_agg_to_history(self, epoch=None):
-        self.agg_history.append(self.get_agg_error())
-        if epoch is not None:
-            self.agg_epochs.append(epoch)
-
-    def get_agg_error_history(self):
-        return self.agg_history
-
-    def update_agg(self, err, numex):
-        self.current_agg_norma += numex
-        err = err * numex if self.aggmode == "mean" else err
-        self.current_agg_error += err
-
-    def _reset(self):  # full reset
-        self.reset_agg()
-        self.agg_history = []
-
-    def reset_agg(self):
-        self.current_agg_error = 0.
-        self.current_agg_norma = 0.
-
-
-class LossAndAgg(Aggregator):
-    """ wraps a loss with aggregator, implements aggregator interface """
-
-    def __init__(self, loss, name=None, mode="mean"):
-        super(LossAndAgg, self).__init__(name=name, mode=mode)
-        self.loss = loss
-        self.set_name(loss.__class__.__name__)
-
-    def __call__(self, pred, gold, **kw):
-        l = self.loss(pred, gold, **kw)
-        numex = pred.size(0) if not q.issequence(pred) else pred[0].size(0)
-        if isinstance(l, tuple) and len(l) == 2:     # loss returns numex too
-            numex = l[1]
-            l = l[0]
-        if isinstance(l, torch.Tensor):
-            lp = l.item()
-        else:
-            lp = l
-        self.update_agg(lp, numex)
-        return l
-
-    def post_agg_epoch(self, x):
-        if hasattr(self.loss, "post_agg_epoch"):
-            x = self.loss.post_agg_epoch(x)
-        return x
-
-    def device(self, device):
-        self.loss.to(device)
-
-    def set_name(self, name):
-        self.name = name
-
-    def get_name(self):
-        return self.name
-
-
 class EventEmitter(object):
 
     def __init__(self, **kw):
@@ -142,64 +59,6 @@ class EventEmitter(object):
             return
         for f in self._event_callbacks[e]:
             f(self)
-
-
-class lossarray(EventEmitter):
-    """ Collection of losses to compute during training, validation or testing
-        First provided loss will be used as training loss when lossarray is used for training.
-        Other losses are there for information purposes.
-
-        Each argument can either be a loss module or a tuple of (loss, tranf)
-            where loss is a loss module and transf is a function applied
-            on the prediction argument before passing it to the loss module itself.
-        Transf is only passed the prediction argument (not gold or input).
-        If transf returns two elements, they are interpreted as prediction and **kw
-            arguments to the loss module (and gold is passed as-is).
-
-        Transf can be of type python function or q.loss_input_transform.
-            In the latter case, there is full control over the inputs as
-            prediction, gold and input arguments are passed to transf.
-    """
-    # TODO: supports normal PyTorch losses?
-    BEFORE_PUSH = 1
-    AFTER_PUSH = 2
-
-    def __init__(self, trainloss, *losses, **kw):
-        super(lossarray, self).__init__()
-        self.losses = []
-        for loss in (trainloss,) + losses:
-            self.losses.append(LossAndAgg(loss))
-
-    def __call__(self, prediction, gold):
-        """ prediction from gold, gold from model """
-        outl = []
-        for loss in self.losses:
-            l = loss(prediction, gold)
-            outl.append(l)
-        return outl
-
-    def get_agg_errors(self):
-        return [loss.get_agg_error() for loss in self.losses]
-
-    def pp(self):
-        aggouts = self.get_agg_errors()
-        ret = " :: ".join(["{:.4f}".format(aggout) for aggout in aggouts])
-        return ret
-
-    def device(self, device):
-        for loss in self.losses:
-            loss.device(device)
-
-    def push_and_reset(self, epoch=None):
-        self.do_callbacks(self.BEFORE_PUSH)
-        for loss in self.losses:
-            loss.push_agg_to_history(epoch=epoch)
-            loss.reset_agg()
-        self.do_callbacks(self.AFTER_PUSH)
-
-    def reset(self):
-        for loss in self.losses:
-            loss._reset()
 
 
 def no_losses(n):
@@ -457,50 +316,16 @@ def pp_epoch_losses(*losses:LossWrapper):
     return ret
 
 
-class trainer(EventEmitter, AutoHooker):
-    """
-    Trainer.
-    Holds a model, lossarray, dataloader, batchtransformers.
-    Supports compatible AutoHookers.
-    """
-    START = 0
-    END = 1
-    START_EPOCH = 2
-    END_EPOCH = 3
-    START_TRAIN = 4
-    END_TRAIN = 5
-    START_BATCH = 8
-    END_BATCH = 9
-    RESET = 15
-    INIT = 16
-    BEFORE_OPTIM_STEP = 12
-    AFTER_OPTIM_STEP = 13
-
+class Modelholder(EventEmitter, AutoHooker):
     def __init__(self, model, **kw):
-        super(trainer, self).__init__(**kw)
+        super(Modelholder, self).__init__(**kw)
         self.model = model
         self.losses = None
-        self.max_epochs = None
-        self.current_epoch = 0
-        self.stop_training = None
         self._device = torch.device("cpu")
-        self.optim = None
         self.transform_batch_inp = None
         self.transform_batch_out = None
         self.transform_batch_gold = None
         self.dataloader = None
-        self.tt = ticktock("trainer")
-
-    def hook(self, f, *es, **kw):
-        # special hooker wrappers
-        if isinstance(f, torch.optim.lr_scheduler._LRScheduler):
-            return super(trainer, self).hook(_LRSchedulerAutoHooker(f, **kw))
-        elif isinstance(f, torch.optim.lr_scheduler.ReduceLROnPlateau):
-            assert(len(es) == 1)
-            return super(trainer, self).hook(_ReduceLROnPlateauAutoHooker(f, es[0]))
-        # normal hooking
-        else:
-            return super(trainer, self).hook(f, *es, **kw)
 
     def device(self, device):
         self._device = device
@@ -537,18 +362,72 @@ class trainer(EventEmitter, AutoHooker):
         assert(all_linear == some_linear)
         return all_linear
 
+    def set_batch_transformer(self, input_transform=None, output_transform=None, gold_transform=None):
+        self.transform_batch_inp = input_transform
+        self.transform_batch_out = output_transform
+        self.transform_batch_gold = gold_transform
+        return self
+
+    def reset(self):
+        for loss in self.losses:
+            loss._reset()
+        self.do_callbacks(self.RESET)
+        return self
+
+    def pre_run(self):
+        self.reset()
+        self.initialize()
+        self.do_callbacks(self.START)
+        for loss in self.losses:
+            loss._reset()
+
+    def post_run(self):
+        self.do_callbacks(self.END)
+
+
+class trainer(Modelholder):
+    """
+    Trainer.
+    Holds a model, lossarray, dataloader, batchtransformers.
+    Supports compatible AutoHookers.
+    """
+    START = 0
+    END = 1
+    START_EPOCH = 2
+    END_EPOCH = 3
+    START_TRAIN = 4
+    END_TRAIN = 5
+    START_BATCH = 8
+    END_BATCH = 9
+    RESET = 15
+    INIT = 16
+    BEFORE_OPTIM_STEP = 12
+    AFTER_OPTIM_STEP = 13
+
+    def __init__(self, model, **kw):
+        super(trainer, self).__init__(model, **kw)
+        self.max_epochs = None
+        self.current_epoch = 0
+        self.stop_training = None
+        self.tt = ticktock("trainer")
+
+    def hook(self, f, *es, **kw):
+        # special hooker wrappers
+        if isinstance(f, torch.optim.lr_scheduler._LRScheduler):
+            return super(trainer, self).hook(_LRSchedulerAutoHooker(f, **kw))
+        elif isinstance(f, torch.optim.lr_scheduler.ReduceLROnPlateau):
+            assert(len(es) == 1)
+            return super(trainer, self).hook(_ReduceLROnPlateauAutoHooker(f, es[0]))
+        # normal hooking
+        else:
+            return super(trainer, self).hook(f, *es, **kw)
+
     def optimizer(self, optimizer):
         self.optim = optimizer
         return self
 
     def epochs(self, epochs):
         self.max_epochs = epochs
-        return self
-
-    def set_batch_transformer(self, input_transform=None, output_transform=None, gold_transform=None):
-        self.transform_batch_inp = input_transform
-        self.transform_batch_out = output_transform
-        self.transform_batch_gold = gold_transform
         return self
 
     # region LOOPS
@@ -682,20 +561,7 @@ class trainer(EventEmitter, AutoHooker):
 
     def reset(self):
         self.current_epoch = 0
-        for loss in self.losses:
-            loss._reset()
-        self.do_callbacks(self.RESET)
-        return self
-
-    def pre_run(self):
-        self.reset()
-        self.initialize()
-        self.do_callbacks(self.START)
-        for loss in self.losses:
-            loss._reset()
-
-    def post_run(self):
-        self.do_callbacks(self.END)
+        super(trainer, self).reset()
 
     def run(self):
         self.pre_run()
@@ -704,9 +570,9 @@ class trainer(EventEmitter, AutoHooker):
 
     # region AutoHooker interface  -- how it hooks into a loop runner
     def get_hooks(self, ee):
-        return {trainer.END_EPOCH: self.on_end_epoch,
-                trainer.RESET: self.on_reset,
-                trainer.INIT: self.on_init}
+        return {ee.END_EPOCH: self.on_end_epoch,
+                ee.RESET: self.on_reset,
+                ee.INIT: self.on_init}
 
     def on_reset(self, owner, **kw):    self.reset()
     def on_init(self, owner, **kw):     self.initialize()
@@ -720,7 +586,7 @@ class trainer(EventEmitter, AutoHooker):
     # endregion
 
 
-class tester(EventEmitter, AutoHooker):
+class tester(Modelholder):
     START = 0
     END = 1
     START_TEST = 4
@@ -735,73 +601,8 @@ class tester(EventEmitter, AutoHooker):
     _name = "tester"
 
     def __init__(self, model, **kw):
-        super(tester, self).__init__(**kw)
-        self.model = model
-        self.losses = None
-        self.transform_batch_inp = None
-        self.transform_batch_out = None
-        self.transform_batch_gold = None
-        self.dataloader = None
+        super(tester, self).__init__(model, **kw)
         self.tt = ticktock(self._name)
-        self._device = torch.device("cpu")
-
-    def device(self, device):
-        self._device = device
-        return self
-
-    def initialize(self):
-        print("WARNING: setting device of model and loss ! (beware: instance might change)")
-        for loss in self.losses:
-            loss.device(self._device)
-        self.model.to(self._device)
-        self.do_callbacks(self.INIT)
-
-    def on(self, dataloader):
-        self.dataloader = dataloader
-        return self
-
-    def loss(self, *args):
-        # can be unspecified
-        if len(args) == 1 and isinstance(args[0], int):
-            args = q.no_losses(args[0])
-
-            self.losses = [LossWrapper(loss) if not isinstance(loss, LossWrapper) else loss for loss in args]
-        return self
-
-    @property
-    def no_gold(self):
-        all_linear = True
-        some_linear = False
-        for loss in self.losses:
-            if isinstance(loss.loss, (q.LinearLoss, q.SelectedLinearLoss)):
-                some_linear = True
-            else:
-                all_linear = False
-        assert(all_linear == some_linear)
-        return all_linear
-
-    def set_batch_transformer(self, input_transform=None, output_transform=None, gold_transform=None):
-        self.transform_batch_inp = input_transform
-        self.transform_batch_out = output_transform
-        self.transform_batch_gold = gold_transform
-        return self
-
-    def reset(self):
-        if self.losses is not None:
-            for loss in self.losses:
-                loss._reset()
-        self.do_callbacks(self.RESET)
-        return self
-
-    def pre_run(self):
-        self.reset()
-        self.initialize()
-        self.do_callbacks(self.START)
-        for loss in self.losses:
-            loss._reset()
-
-    def post_run(self):
-        self.do_callbacks(self.END)
 
     def run(self):
         self.pre_run()
@@ -846,7 +647,7 @@ class tester(EventEmitter, AutoHooker):
             modelout2loss = modelouts
             if self.transform_batch_out is not None:
                 modelout2loss = self.transform_batch_out(modelouts)
-            gold = batch[-1]
+
             if self.transform_batch_gold is not None:
                 gold = self.transform_batch_gold(gold)
 
@@ -882,9 +683,9 @@ class tester(EventEmitter, AutoHooker):
 
     # region AutoHooker -- how it hooks into a trainer
     def get_hooks(self, ee):
-        return {trainer.END_TRAIN: self.on_end_epoch,
-                trainer.RESET: self.on_reset,
-                trainer.INIT: self.on_init}
+        return {ee.END_TRAIN: self.on_end_epoch,
+                ee.RESET: self.on_reset,
+                ee.INIT: self.on_init}
 
     def on_reset(self, owner, **kw):    self.reset()
     def on_init(self, owner, **kw):     self.initialize()
