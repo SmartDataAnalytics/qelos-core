a8bafe3f9887593bf09229f7fcc58d68e69d032f
lukovnikov <lukovnikov@outlook.com>
2018-10-24 19:44:29 +0200

    deep_copy

--- qelos_core/scripts/lm/transformer_lm.py
+++ qelos_core/scripts/lm/transformer_lm.py
@@ -319,7 +319,7 @@ def run(lr=0.001,
     lrp = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, mode="min", factor=1/2, patience=1, verbose=True)
 
     trainer = q.trainer(m).on(train_batches).loss(loss).optimizer(optim).device(device).hook(m).hook(gradclip)
-    tester = q.tester(valid_m).on(valid_batches).loss(loss, ppl_loss).device(device).hook(m)
+    tester = q.tester(valid_m).on(valid_batches).loss(test_loss, ppl_loss).device(device).hook(m)
 
     tt.tock("created model")
     tt.tick("training")
